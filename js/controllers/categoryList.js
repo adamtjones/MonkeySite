4868e0dd23eeae985be7609b9f91ecf4df33dda3
Adam Jones <adamjns802@gmail.com>
2016-07-28 12:16:28 -0400

    minor updates

--- js/controllers/categoryList.js
+++ js/controllers/categoryList.js
@@ -3,7 +3,22 @@
     
     angular
     .module('taco')
-    .controller('categoryController', function(API) {
+    .controller('categoryController', function category() {
        var vm = this;
+
+       var category1 = {
+       	name: "Chimpanzees",
+       }
+
+       var category2 = {
+       	name:"Monkeys",
+       }
+
+       var category3 = {
+       	name:"Gorillas",
+       }
+
+       vm.data = [category1, category2, category3];
+
        });
 })();
